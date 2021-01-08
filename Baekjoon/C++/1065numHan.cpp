c9e9fce7460b7b053937ad7ca3b406ffa2fec36d
WoosungMichael <woosung0420k@naver.com>
2021-01-08 22:51:26 +0900

    21/01/01 solved numHan 한수

---
+++ Baekjoon/C++/1065numHan.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int N;
+	cin >> N;
+	int a, b, c;
+	int cnt = 0;
+	
+	if (N < 100) {
+		cnt += N;
+	}
+	else{
+		cnt += 99;
+		for (int i = 100; i <= N; i++) {
+			a = i / 100;
+			b = (i - a * 100) / 10;
+			c = i % 10;
+			if (b - a == c - b)
+				cnt++;
+		}
+	}
+
+	cout << cnt;
+}
