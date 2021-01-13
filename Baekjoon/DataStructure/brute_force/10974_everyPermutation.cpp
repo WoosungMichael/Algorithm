07336f2b3c7565f0b7a7e133f71473714d490840
WoosungMichael <woosung0420k@naver.com>
2021-01-13 23:31:30 +0900

    21/01/13 solved everyPermutation 모든 순열 (brute_force)

---
+++ Baekjoon/DataStructure/brute_force/10974_everyPermutation.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+
+    vector<int> v;
+    for (int i = 0; i < n; i++) {
+        v.push_back(i + 1);
+    }
+
+    do {
+        for (int i = 0; i < n; i++) {
+            cout << v[i] << " ";
+        }
+        cout << "\n";
+    } while (next_permutation(v.begin(), v.end()));
+
+}
