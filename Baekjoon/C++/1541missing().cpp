8413ac7e5a76c3a2a4bdf80e4a74380473677320
WoosungMichael <woosung0420k@naver.com>
2021-02-06 02:08:56 +0900

    21/02/04 solved missing() 잃어버린 괄호 <string에서 stoi로 숫자처리>

--- Baekjoon/C++/1541missing().cpp
+++ Baekjoon/C++/1541missing().cpp
@@ -7,12 +7,12 @@ int main() {
 	string line;
 	cin >> line;
 
-	vector<int>num(50, 0);
+	vector<int>num(50,0);
 	int index = 0;
 
 	bool isNum = false;
 	for (int i = 0; i < line.length(); i++) {
-
+		
 		if (line[i] == '+') {
 			isNum = false;
 		}
@@ -22,7 +22,7 @@ int main() {
 		}
 		else {
 			if (isNum == false) {
-				num[index] += (stoi(&line[i]));
+				num[index] += (stoi(&line[i])); //***현재 인덱스 위치부터 시작하는 수 전체를 읽어옴***
 				isNum = true;
 			}
 		}
