794e6e3c1e4d818b1e8e9c7070b41099448d212d
WoosungMichael <woosung0420k@naver.com>
2021-02-12 21:38:13 +0900

    21/02/12 solved gettingMiddleWord 가운데 글자 가져오기

---
+++ Programmers/C++/Level_1/gettingMiddleWord.cpp
@@ -0,0 +1,14 @@
+#include <string>
+
+using namespace std;
+
+string solution(string s) {
+    string answer = "";
+    if (s.length() % 2 != 0)
+        answer = s[s.length() / 2];
+    else {
+        answer += s[s.length() / 2 - 1];
+        answer += s[s.length() / 2];
+    }
+    return answer;
+}
