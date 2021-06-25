b53d6a38aa93fcbf592334e4763922e7e5261208
WoosungMichael <woosung0420k@naver.com>
2021-06-25 23:07:52 +0900

    21/06/25 solved Y2

--- DavinciCoding/Y1.py
+++ DavinciCoding/Y1.py
@@ -1,8 +1,7 @@
-num=input()
-
+num = input()
 answer = int(num[0])
 for i in range(len(num)-1):
-    if(answer<=1 or int(num[i+1])<=1):
+    if(answer <= 1 or int(num[i+1]) <= 1):
         answer += int(num[i+1])
     else:
         answer *= int(num[i+1])
