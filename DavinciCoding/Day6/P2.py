19d1cacdf55fd438ea237e7eaea8dd767667f70f
WoosungMichael <woosung0420k@naver.com>
2021-07-06 17:13:49 +0900

    21/07/06 solved P2

--- DavinciCoding/Day6/P2.py
+++ DavinciCoding/Day6/P2.py
@@ -6,7 +6,7 @@ tmp = 30000
 
 def action(x, tmp):
     if(x == 1):
-        return 1
+        return 0
     if(x % 5 == 0):
         if(tmp > 1+action(x/5, tmp)):
             tmp = 1+action(x/5, tmp)
@@ -22,4 +22,21 @@ def action(x, tmp):
     return tmp
 
 
-print(action(x, tmp)-1)
+print(action(x, tmp))
+
+
+'''
+x = int(input())
+a = [0 for _ in range(x+1)]
+a[1] = 0
+
+for i in range(2, x+1):
+    a[i] = a[i-1]+1
+    if i % 5 == 0:
+        a[i] = min(a[i//5]+1, a[i])
+    if i % 3 == 0:
+        a[i] = min(a[i//3]+1, a[i])
+    if i % 2 == 0:
+        a[i] = min(a[i//2]+1, a[i])
+print(a[x])
+'''
