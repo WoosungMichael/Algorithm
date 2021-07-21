9d6c9d2b1c60da9556d28dfa29631ac42f85bb4a
Woosung Kim <woosung0420k@naver.com>
2021-07-21 23:58:37 +0900

    21/07/21 solved polyomino 폴리오미노

---
+++ Baekjoon/Python/1343_greedy_polyomino.py
@@ -0,0 +1,33 @@
+board = input()
+
+cnt = 0
+answer = ""
+for i in range(len(board)):
+    if(board[i] == 'X'):
+        cnt += 1
+    else:
+        while(cnt > 0):
+            if(cnt >= 4):
+                answer += "AAAA"
+                cnt -= 4
+            elif(cnt == 2):
+                answer += "BB"
+                cnt -= 2
+            else:
+                print(-1)
+                exit()
+        answer += "."
+
+    if(i == len(board)-1):
+        while(cnt > 0):
+            if(cnt >= 4):
+                answer += "AAAA"
+                cnt -= 4
+            elif(cnt == 2):
+                answer += "BB"
+                cnt -= 2
+            else:
+                print(-1)
+                exit()
+
+print(answer)
