00c37519f3048a5ac51b123a886e646b4a98aeae
Woosung Kim <woosung0420k@naver.com>
2021-07-21 23:59:27 +0900

    21/07/21 solved change 거스름돈

---
+++ Baekjoon/Python/14916_greedy_change.py
@@ -0,0 +1,27 @@
+change = int(input())
+
+if(change == 1 or change == 3):
+    cnt = -1
+else:
+    if(change % 10 == 0):
+        cnt = change//5
+    elif(change % 10 == 1):
+        cnt = (change-6)//5 + 3
+    elif(change % 10 == 2):
+        cnt = (change-2)//5 + 1
+    elif(change % 10 == 3):
+        cnt = (change-8)//5 + 4
+    elif(change % 10 == 4):
+        cnt = (change-4)//5 + 2
+    elif(change % 10 == 5):
+        cnt = change//5
+    elif(change % 10 == 6):
+        cnt = (change-6)//5 + 3
+    elif(change % 10 == 7):
+        cnt = (change-2)//5 + 1
+    elif(change % 10 == 8):
+        cnt = (change-8)//5 + 4
+    elif(change % 10 == 9):
+        cnt = (change-4)//5 + 2
+
+print(cnt)
