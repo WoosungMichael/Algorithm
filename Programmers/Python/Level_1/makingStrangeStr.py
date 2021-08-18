38d8a3bbc7ad0cc618e64b5ff9f0873536f722a1
Woosung Kim <woosung0420k@naver.com>
2021-08-18 23:45:56 +0900

    21/08/18 solved makingStrangeStr 이상한 문자 만들기

---
+++ Programmers/Python/Level_1/makingStrangeStr.py
@@ -0,0 +1,14 @@
+def solution(s):
+    answer = ''
+    cnt = 0
+    for i in s:
+        if(i == ' '):
+            answer += i
+            cnt = 0
+        elif(cnt % 2 == 0):
+            answer += i.upper()
+            cnt += 1
+        else:
+            answer += i.lower()
+            cnt += 1
+    return answer
