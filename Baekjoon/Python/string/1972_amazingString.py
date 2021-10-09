e6b0cacaafabf9fc686de4fc2d6781c3eec330fb
Woosung Kim <woosung0420k@naver.com>
2021-10-10 01:52:44 +0900

    21/10/10 solved amazingString 놀라운 문자열

--- Baekjoon/Python/string/1972_amazingString.py
+++ Baekjoon/Python/string/1972_amazingString.py
@@ -1,18 +1,21 @@
 str = input()
 
 while (str != "*"):
-    for i in range(1, len(str)-1):
+    flag = True
+    for i in range(1, len(str)):
         tmp = []
         tmp_s = ""
         index = 0
-        flag = True
+
         while index + i < len(str):
             tmp_s = str[index] + str[index + i]
             tmp.append(tmp_s)
             index += 1
+
         if (len(tmp) != len(list(set(tmp)))):
             flag = False
             break
+
     if flag:
         print(str, end="")
         print(" is surprising.")
