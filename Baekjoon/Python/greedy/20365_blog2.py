3cb6d9245aadfb4aebe3a2317a9966b096194445
Woosung Kim <woosung0420k@naver.com>
2021-10-14 15:14:38 +0900

    21/10/14 update blog2 블로그2

--- Baekjoon/Python/greedy/20365_blog2.py
+++ Baekjoon/Python/greedy/20365_blog2.py
@@ -1,7 +1,21 @@
 N = int(input())
 str = input()
 
-tmp = str[0]
+cnt_B = 0
+cnt_R = 0
+tmp = 'a'
+for i in str:
+    if i == 'B' and tmp != 'B':
+        cnt_B += 1
+        tmp = 'B'
+    elif i == 'R' and tmp != 'R':
+        cnt_R += 1
+        tmp = 'R'
+
+if cnt_B > cnt_R:
+    tmp = 'B'
+else:
+    tmp = 'R'
 cnt = 1
 flag = 1
 for i in str:
