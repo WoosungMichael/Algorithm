33a806230effa97958a32f6d00cabb17cc880d2d
Woosung Kim <woosung0420k@naver.com>
2021-10-31 19:41:49 +0900

    21/10/31 solved organizeFile 파일 정리

--- Baekjoon/Python/string/20291_organizeFile.py
+++ Baekjoon/Python/string/20291_organizeFile.py
@@ -1,28 +1,15 @@
 N = int(input())
 
-arr = []
+arr = dict()
 for i in range(N):
-    tmp = input()
-    flag = False
-    tmp_w = ""
-    for j in tmp:
-        if flag:
-            tmp_w += j
-        if j == '.':
-            flag = True
-    arr.append(tmp_w)
-
-arr.sort()
-
-cnt = 1
-flag = True
-for i in range(len(arr)):
-    if flag:
-        print(arr[i], end=" ")
-        flag = False
-    if i == len(arr)-1 or arr[i] != arr[i+1]:
-        print(cnt)
-        cnt = 1
-        flag = True
+    tmp = input().split('.')[1]
+    if tmp not in arr:
+        arr[tmp] = 1
     else:
-        cnt += 1
+        arr[tmp] += 1
+
+sorted_arr = sorted(arr.items())
+
+
+for key, value in sorted_arr:
+    print(key, value)
