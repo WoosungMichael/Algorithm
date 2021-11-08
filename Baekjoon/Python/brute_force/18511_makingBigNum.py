896d2288053ef6423eebd6cec1c36fd0be7e53d1
Woosung Kim <woosung0420k@naver.com>
2021-11-08 14:12:28 +0900

    21/11/08 solved makingBigNum 큰 수 구성하기

--- Baekjoon/Python/brute_force/18511_makingBigNum.py
+++ Baekjoon/Python/brute_force/18511_makingBigNum.py
@@ -1,12 +1,12 @@
 from itertools import product
 
-N, K = map(int, input().split())
+N, K = input().split()
 arr = list(input().split())
 
-for i in reversed(range(int(K)+1)):
+for i in reversed(range(len(N)+1)):
     tmp = list(product(arr, repeat=i))
     tmp.sort(reverse=True)
     for i in tmp:
-        if int("".join(i)) <= N:
+        if int("".join(i)) <= int(N):
             print(int("".join(i)))
             exit()
