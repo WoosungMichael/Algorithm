68b207c48fa287316a6ca5860e54e12a9b0546e9
Woosung Kim <woosung0420k@naver.com>
2021-12-24 21:09:50 +0900

    21/12/24 solved pokemonMaster 나는야 포켓몬 마스터 이다솜

--- Baekjoon/Python/data_structure2/1620_pokemonMaster.py
+++ Baekjoon/Python/data_structure2/1620_pokemonMaster.py
@@ -1,17 +1,19 @@
+import sys
+input = sys.stdin.readline
+
 N, M = map(int, input().split())
 
-pokemon = {}
-num = {}
-index = 1
+pokemon = []
+pokemon_dict = {}
+
 for i in range(N):
-    monster = input()
-    pokemon[index] = monster
-    num[monster] = index
-    index += 1
+    monster = input().rstrip()
+    pokemon.append(monster)
+    pokemon_dict[monster] = i + 1
 
 for i in range(M):
-    Q = input()
+    Q = input().rstrip()
     if 49 <= ord(Q[0]) <= 57:
-        print(pokemon[int(Q)])
+        print(pokemon[int(Q)-1])
     else:
-        print(num[Q])
+        print(pokemon_dict[Q])
