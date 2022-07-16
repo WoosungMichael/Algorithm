7a77007355713487e7af6a13e2199c3b66fb1106
Woosung Kim <woosung0420k@naver.com>
2022-07-16 21:01:49 +0900

    22/07/16 solving 팩토리얼 0의 개수

---
+++ Baekjoon/Python/math/1676_0inFactorial.py
@@ -0,0 +1,16 @@
+def factorial(n):
+    if n <= 1:
+        return 1
+    else:
+        return n * factorial(n - 1)
+    
+N = int(input())
+answer = 0
+if N <= 1:
+    print(0)
+else:
+    num = factorial(N)
+    while num % 10 == 0:
+        answer += 1
+        num /= 10
+    print(answer)
