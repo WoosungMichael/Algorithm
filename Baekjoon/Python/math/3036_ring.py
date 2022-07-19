152f3a501b8c556b885193d122e9926c9f7593e0
Woosung Kim <woosung0420k@naver.com>
2022-07-20 00:09:58 +0900

    22/07/20 solved 링

--- Baekjoon/Python/math/3036_ring.py
+++ Baekjoon/Python/math/3036_ring.py
@@ -1,10 +1,18 @@
+def div(a, b):
+    min_num = min(a, b)
+    max_num = max(a, b)
+    for i in range(min_num, 0, -1):
+        if max_num % i == 0 and min_num % i == 0:
+            return a // i, b // i
+
 N = int(input())
 
 arr = list(map(int, input().split()))
 ring = arr[0]
 
 for i in range(1, len(arr)):
-    print("", end = "")
+    x, y = div(ring, arr[i])
+    print(x, end = "")
     print("/", end = "")
-    print("", end = "")
+    print(y)
     
